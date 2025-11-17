26a52561f0fb99ba95e48beba9cdd4c72d43007f
laxmimerit <laxmimerit@gmail.com>
2025-11-18 01:16:22 +0530

    simplified mysql mcp code

--- 13. LangGraph MCP/mysql_mcp.py
+++ 13. LangGraph MCP/mysql_mcp.py
@@ -20,35 +20,40 @@ BASE_URL = "http://localhost:11434"
 
 llm = ChatOllama(model=LLM_MODEL, base_url=BASE_URL, temperature=0)
 
+async def get_tools(): 
+    client = MultiServerMCPClient(
+        {
+            "sqlite": {
+                "command": "uvx",
+                "args": [
+                    "git+https://github.com/laxmimerit/mcp-server-sqlite.git",
+                    "--db-path",
+                    "D:\\LLM\\Agentic-RAG-with-LangGraph-and-Ollama\\11. MySQL Agent\\db\\employees_db-full-1.0.6.db"
+                ],
+                'transport': 'stdio'
+            }
+        }
+    )
+    tools = await client.get_tools()
+    print(f"Loaded {len(tools)}")
+    print(f"Tools: {tools}")
+
+    return tools
+
 class AgentState(TypedDict):
     messages: Annotated[list, operator.add]
 
-# make sure to install uv
+# create in-function agent node
+async def agent(state: AgentState):
+    tools = await get_tools()
+    llm_with_tools = llm.bind_tools(tools)
+    response = llm_with_tools.invoke(state['messages'])
+    return {'messages': [response]}
+    
+# make sure to install uvx 
 async def create_agent():
-
-    client = MultiServerMCPClient(
-    {
-        "sqlite": {
-			"command": "uvx",
-			"args": [
-				"git+https://github.com/laxmimerit/mcp-server-sqlite.git",
-				"--db-path",
-				"D:\\LLM\\Agentic-RAG-with-LangGraph-and-Ollama\\11. MySQL Agent\\db\\employees_db-full-1.0.6.db"
-			],
-            'transport': 'stdio'
-		}
-    }
-)
     
-    tools = await client.get_tools()
-    print(f"Loaded {len(tools)} Tools.: {tools}")
-
-    
-    # create in-function agent node
-    def agent(state: AgentState):
-        llm_with_tools = llm.bind_tools(tools)
-        response = llm_with_tools.invoke(state['messages'])
-        return {'messages': [response]}
+    tools = await get_tools()
 
     builder = StateGraph(AgentState)
     builder.add_node('agent', agent)
